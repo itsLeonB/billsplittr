f384b186b9f3a2478027efe4980edf4aca37e659
itsLeonB <ellionblessan@gmail.com>
2024-12-17 22:09:30 +0700

    init spring project

---
+++ src/main/java/com/itsleonb/billsplittr/BillsplittrApplication.java
@@ -0,0 +1,13 @@
+package com.itsleonb.billsplittr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillsplittrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillsplittrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itsleonb/billsplittr/BillsplittrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itsleonb.billsplittr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillsplittrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
