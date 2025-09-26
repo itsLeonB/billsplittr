c33a4a063700981bc54ed81005642fdb6551d40a
itsLeonB <ellionblessan@gmail.com>
2025-09-26 17:47:10 +0700

    fix: load storage config

--- internal/config/config.go
+++ internal/config/config.go
@@ -31,9 +31,13 @@ func Load() Config {
 	var valkey Valkey
 	envconfig.MustProcess("VALKEY", &valkey)
 
+	var storage Storage
+	envconfig.MustProcess("STORAGE", &storage)
+
 	return Config{
-		App:    app,
-		DB:     db,
-		Valkey: valkey,
+		App:     app,
+		DB:      db,
+		Valkey:  valkey,
+		Storage: storage,
 	}
 }
